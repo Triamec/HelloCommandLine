ea7e5ea53b53b151cd24f7516fb15c3c6fa7941b
ls-triamec <larissa.stadelmann@triamec.com>
2025-05-13 15:56:28 +0200

    Main überarbeitet

--- HelloCommandLine/Program.cs
+++ HelloCommandLine/Program.cs
@@ -3,20 +3,17 @@ using Triamec.Tam;
 
 namespace Triamec.Tam.Samples {
     internal class Program {
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         /// <param name="args"></param>
-
         static void Main(string[] args) {
-            //using ConsoleApplication app = new ConsoleApplication();
-            //app.StartUp();
-            //app.CommandLoop();
 
             using StateMachine app = new StateMachine();
+
             app.StateHandler();
+
         }
-
-
     }
 }
